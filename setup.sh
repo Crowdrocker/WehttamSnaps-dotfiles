7d3b163beb5d0d51961859aa05905d2b16ba66b2
Matthew B. <131808292+Crowdrocker@users.noreply.github.com>
2025-09-04 22:05:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -454,6 +454,134 @@ for app in "${FLATPAKS[@]}"; do
     flatpak install -y flathub "$app" || echo -e "${YELLOW}Failed to install $app${NC}"
 done
 
+sleep 1
+clear
+# ----------------------------------------------------------
+# Apply oh-my-zsh and plugins
+# ----------------------------------------------------------
+
+info_message "Installing Oh My Zsh and plugins..."
+
+sleep 1
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Applying plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+success_message "Oh My Zsh and plugins installed successfully."
+
+sleep 1
+clear
+
+
+# ----------------------------------------------------------
+# Download and apply plymouth boot animation
+# ----------------------------------------------------------
+
+info_message "Downloading and applying Plymouth boot animation..."
+
+sleep 1
+
+git clone https://github.com/MrE8065/PSLinux.git ~/PSLinux
+sudo cp -r ~/PSLinux/pslinux /usr/share/plymouth/themes
+sudo plymouth-set-default-theme -R pslinux
+
+success_message "Plymouth boot animation downloaded successfully."
+
+sleep 2
+clear
+
+info_message "Do you want to add the plymouth entry to the mkinitcpio file? If you select no, REMEMBER TO ADD IT TO SHOW THE CUSTOM BOOT ANIMATION (y/n, default:y): "
+read mkinit_choice
+mkinit_choice=${mkinit_choice:-y}
+
+if [[ "$mkinit_choice" == "y" ]]; then
+  info_message "Creating a backup of mkinitcpio.conf."
+  sudo cp /etc/mkinitcpio.conf /etc/mkinitcpio_backup.conf
+  info_message "REMEMBER TO DELETE IT IF THE INSTALLATION IS SUCCESSFUL"
+  sleep 2
+
+  # Script to autodetect if the user is using systemd or udev and add plymouth entry accordingly
+  HOOKS_LINE=$(grep "^HOOKS=" /etc/mkinitcpio.conf)
+
+  if [[ "$HOOKS_LINE" == *"udev"* ]]; then
+    info_message "udev detected"
+    NEW_LINE=$(echo "$HOOKS_LINE" | sed -E 's/(udev)/\1 plymouth/')
+  elif [[ "$HOOKS_LINE" == *"systemd"* ]]; then
+    info_message "systemd detected"
+    NEW_LINE=$(echo "$HOOKS_LINE" | sed -E 's/(systemd)/\1 plymouth/')
+  else
+    info_message "No udev or systemd found in HOOKS. Aborting."
+    exit 1
+  fi
+
+  # Replace HOOKS line with the modified one
+  sudo sed -i "s|^HOOKS=.*|$NEW_LINE|" /etc/mkinitcpio.conf
+
+  info_message "HOOKS line updated."
+  grep "^HOOKS=" /etc/mkinitcpio.conf
+
+  # Regenerate initramfs
+  info_message "Regenerating initramfs images..."
+  sudo mkinitcpio -P
+  success_message "Plymouth entry successfully added to the mkinitcpio.conf file."
+  info_message "Remember to add splash to your kernel parameters to see the boot animation."
+  info_message "See the archwiki entry for Plymouth for more information."
+  sleep 2
+else
+  info_message "Skipping mkinitcpio modification." 
+  info_message "REMEMBER TO ADD PLYMOUTH ENTRY MANUALLY TO SHOW THE CUSTOM BOOT ANIMATION."
+  info_message "ALSO ADD SPLASH TO THE KERNEL PARAMETERS TO SEE THE ANIMATION."
+  info_message "See the archwiki entry for Plymouth for more information."
+  sleep 2
+fi
+
+sleep 2
+clear
+
+
+# ----------------------------------------------------------
+# Download and apply sddm theme
+# ----------------------------------------------------------
+
+info_message "Do you want to install and apply the sddm theme? (y/n, default:y): "
+read sddm_choice
+sddm_choice=${sddm_choice:-y} # Default to 'y' if empty
+
+if [[ "$sddm_choice" == "y" ]]; then
+  info_message "Downloading SDDM theme..."
+
+  sleep 1
+
+	installPackages "sddm-silent-theme"
+
+  sleep 1
+
+  success_message "SDDM theme downloaded successfully."
+
+  sleep 1
+
+  info_message "Applying SDDM theme..."
+
+  sleep 1
+
+  sudo tee /etc/sddm.conf > /dev/null << 'EOF'
+[Theme]
+Current=silent
+
+[General]
+InputMethod=qtvirtualkeyboard
+GreeterEnvironment=QML2_IMPORT_PATH=/usr/share/sddm/themes/silent/components/,QT_IM_MODULE=qtvirtualkeyboard
+EOF
+
+  success_message "SDDM theme applied successfully."
+fi
+
+sleep 1
+clear
 
 # Directories ----------------------------
 hypr_dir="$HOME/.hyprconf/hypr"
