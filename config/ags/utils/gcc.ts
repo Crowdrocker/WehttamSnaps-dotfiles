3ccdff00fdff1f9418530d3bb40297532dd02c19
Matthew B. <berksapexlegends@gmail.com>
2025-09-12 12:55:33 -0500

    update

--- config/ags/utils/gcc.ts
+++
@@ -1,6 +0,0 @@
-import { exec } from "astal";
-
-export async function compileBinaries()
-{
-    exec(`bash -c "mkdir -p ./assets/binaries && gcc -o ./assets/binaries/bandwidth ./scripts/bandwidth.c"`);
-}
